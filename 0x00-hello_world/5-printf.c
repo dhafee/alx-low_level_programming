774dd3c562c1bd0dd2fd93d6783534481eff602f
divine <divineekoh@gmail.com>
2023-06-18 15:13:56 -0700

    hello,prinf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,13 @@
 
 /**
  * main - prints "with proper grammar, but the outcome is 
- * a piece of art," new line
- * Return: 0.
+ * a piece of art," followed  newline
+ * Return: Always 0.
  *
  */
 
 int main(void)
+
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
