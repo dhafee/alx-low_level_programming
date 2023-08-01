25b3cb9de6c5ab5d5dd7f74aa08be10cd09971cc
dhafee <divineekoh@gmail.com>
2023-07-31 17:44:10 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked lists
+ * listint_len - returns the number of elements in a linked listis
  * @h: linked list of type listint_t to traverse
  *
  * Return: number of nodes
--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -53,10 +53,10 @@ size_t looped_listint_len(const listint_t *head)
 }
 
 /**
- * print_listint_safe - prints a listint_t list safely.
- * @head: A pointer to the head of the listint|_list.
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the head of the listint_t list.
  *
- * return: The number of nodes in the list.
+ * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
